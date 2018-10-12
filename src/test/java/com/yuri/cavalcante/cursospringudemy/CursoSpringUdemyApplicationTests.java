76299241b229349a87aacd27cbfeb993519722be
yuricavalcantedev <yuricavalcante17@gmail.com>
2018-10-12 16:10:47 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/yuri/cavalcante/cursospringudemy/CursoSpringUdemyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yuri.cavalcante.cursospringudemy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoSpringUdemyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
