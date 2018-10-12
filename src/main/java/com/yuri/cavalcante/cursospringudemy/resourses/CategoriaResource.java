b3b1aa425ddb39ce1229473e8ae27856b4042846
yuricavalcantedev <yuricavalcante17@gmail.com>
2018-10-12 16:42:02 -0300

    Testando Categoria

--- src/main/java/com/yuri/cavalcante/cursospringudemy/resourses/CategoriaResource.java
+++ src/main/java/com/yuri/cavalcante/cursospringudemy/resourses/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.yuri.cavalcante.cursospringudemy.resourses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yuri.cavalcante.cursospringudemy.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST is working";
+	public List<Categoria> listar() {
+		
+		Categoria categoria = new Categoria(1,"Informática");
+		Categoria categoria2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> listCategoria = new ArrayList<Categoria>();
+		listCategoria.add(categoria);
+		listCategoria.add(categoria2);
+		
+		return listCategoria;
 	}
 }
