39a032b049a9307c5c45a81a9f84c7a6a3e3f9ad
yuricavalcantedev <yuricavalcante17@gmail.com>
2018-10-15 16:13:36 -0300

    Adicionando Pages ao Categoria

--- src/main/java/com/yuri/cavalcante/cursospringudemy/resourses/CategoriaResource.java
+++ src/main/java/com/yuri/cavalcante/cursospringudemy/resourses/CategoriaResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -38,6 +40,7 @@ public class CategoriaResource {
 		
 		return ResponseEntity.ok().body(categoriasDTO);
 	}
+	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 
@@ -72,5 +75,19 @@ public class CategoriaResource {
 		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@GetMapping(value="/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue="0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue="nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue="ASC") String direction) {
+		
+		Page<Categoria> categorias = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		
+		Page<CategoriaDTO> categoriasDTO = categorias.map(categoria -> new CategoriaDTO(categoria));
+		
+		return ResponseEntity.ok().body(categoriasDTO);
 	}
 }
