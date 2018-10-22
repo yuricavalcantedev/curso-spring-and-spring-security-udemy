ea3805780e4a082482c5a16937fdac37ad800d28
yuricavalcantedev <yuricavalcante17@gmail.com>
2018-10-22 16:35:52 -0300

    Validação customizada para o email

--- src/main/java/com/yuri/cavalcante/cursospringudemy/repositories/ClienteRepository.java
+++ src/main/java/com/yuri/cavalcante/cursospringudemy/repositories/ClienteRepository.java
@@ -2,6 +2,8 @@ package com.yuri.cavalcante.cursospringudemy.repositories;
 
 import java.io.Serializable;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,7 @@ import com.yuri.cavalcante.cursospringudemy.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Serializable>{
 
+	@Transactional
+	Cliente findByEmail(String email);
+	
 }
--- src/main/java/com/yuri/cavalcante/cursospringudemy/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/yuri/cavalcante/cursospringudemy/services/validation/ClienteInsertValidator.java
@@ -6,27 +6,39 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.yuri.cavalcante.cursospringudemy.domain.Cliente;
 import com.yuri.cavalcante.cursospringudemy.domain.enums.TipoCliente;
 import com.yuri.cavalcante.cursospringudemy.dto.ClienteNewDTO;
+import com.yuri.cavalcante.cursospringudemy.repositories.ClienteRepository;
 import com.yuri.cavalcante.cursospringudemy.resourses.exceptions.FieldMessage;
 import com.yuri.cavalcante.cursospringudemy.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteNewDTO objDTO, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 
-		if(objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+		if(objDTO.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
 		}
 		
-		if(objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+		if(objDTO.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));
+		}
+		
+		Cliente clienteAux = clienteRepository.findByEmail(objDTO.getEmail());
+		if(clienteAux != null) {
+			list.add(new FieldMessage("email","Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
